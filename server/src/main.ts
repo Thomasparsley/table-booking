629cdb334367edcb57eee0bbc39ed893c88dd880
Thomasparsley <info@thomasparsley.cz>
2022-11-25 14:01:53 +0100

    server main file init
    Co-authored-by: petrspirka <petrspirka@users.noreply.github.com>
    Co-authored-by: Marek Brodacký <slunimara@users.noreply.github.com>
    Co-authored-by: MatyasHroch <MatyasHroch@users.noreply.github.com>

---
+++ server/src/main.ts
@@ -0,0 +1,18 @@
+import express, { Express } from "express";
+
+const app: Express = express();
+
+app.get("/ping", function (_, res) {
+    res.status(200).write("Pong!");
+});
+
+let port: Number;
+
+if (process.env.IS_PRODUCTION) {
+    port = 3000;
+}
+else {
+    port = 4000;
+}
+
+app.listen(port);
