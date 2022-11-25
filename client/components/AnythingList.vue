412fcc761203e059e4b19501e6074fff9494735b
Thomasparsley <info@thomasparsley.cz>
2022-11-25 14:02:10 +0100

    client better init

---
+++ client/components/AnythingList.vue
@@ -0,0 +1,18 @@
+<script>
+
+export default {
+    name: 'AnythingList',
+    props: [items]
+}
+
+</script>
+
+<template>
+    <ul>
+        <li v-for="item in items" :key="item.id">
+            <span v-for="(value, key) in item" :key="key">
+                {{ key }}: {{ value }}
+            </span>
+        </li>
+    </ul>
+</template>
---
+++ client/pages/index.vue
@@ -0,0 +1,6 @@
+<template>
+    <div>
+        <h1>Welcome to the homepage</h1>
+        <span>Hele, sem muzem neco dat kdyztak</span>
+    </div>
+</template>
