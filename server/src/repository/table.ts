075cb1d221f9cb295cea3ef88e2d04055074d45e
Thomasparsley <info@thomasparsley.cz>
2022-11-25 16:31:58 +0100

    table repository
    Co-authored-by: petrspirka <petrspirka@users.noreply.github.com>
    Co-authored-by: Marek Brodacký <slunimara@users.noreply.github.com>
    Co-authored-by: MatyasHroch <MatyasHroch@users.noreply.github.com>

--- server/src/repository/table.ts
+++ server/src/repository/table.ts
@@ -1,15 +1,30 @@
-import { TableModel, ITable } from "../schemas";
+import { ITable, TableModel } from "../schemas";
+import { DtoNewTable, DtoUpdateTable } from "../dtos";
+
 
 export class TableRepository {
     constructor(
         private readonly tableModel: typeof TableModel,
     ) { }
 
-    async getById(id: string): Promise<ITable> {
+    async getById(id: string): Promise<ITable | null> {
         return await this.tableModel.findOne({ _id: id });
     }
 
     async getAll(): Promise<ITable[]> {
         return await this.tableModel.find();
+    }
+
+    async create(user: DtoNewTable): Promise<ITable> {
+        const newUser = new this.tableModel(user);
+        return await newUser.save();
+    }
+
+    async update(id: string, user: DtoUpdateTable): Promise<ITable | null> {
+        return await this.tableModel.findOneAndUpdate({ _id: id }, user, { new: false });
+    }
+
+    async delete(id: string): Promise<ITable | null> {
+        return await this.tableModel.findOneAndDelete({ _id: id });
     }
 }
