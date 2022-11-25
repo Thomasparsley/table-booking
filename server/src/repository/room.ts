f33cc62f09d5e9823e4eab4d325df92e069dd7c9
Thomasparsley <info@thomasparsley.cz>
2022-11-25 16:31:49 +0100

    room repository
    Co-authored-by: petrspirka <petrspirka@users.noreply.github.com>
    Co-authored-by: Marek Brodacký <slunimara@users.noreply.github.com>
    Co-authored-by: MatyasHroch <MatyasHroch@users.noreply.github.com>

---
+++ server/src/repository/room.ts
@@ -0,0 +1,30 @@
+import { IRoom, RoomModel } from "../schemas";
+import { DtoNewRoom, DtoUpdateRoom } from "../dtos";
+
+
+export class RoomRepository {
+    constructor(
+        private readonly roomModel: typeof RoomModel,
+    ) { }
+
+    async getById(id: string): Promise<IRoom | null> {
+        return await this.roomModel.findOne({ _id: id });
+    }
+
+    async getAll(): Promise<IRoom[]> {
+        return await this.roomModel.find();
+    }
+
+    async create(user: DtoNewRoom): Promise<IRoom> {
+        const newUser = new this.roomModel(user);
+        return await newUser.save();
+    }
+
+    async update(id: string, user: DtoUpdateRoom): Promise<IRoom | null> {
+        return await this.roomModel.findOneAndUpdate({ _id: id }, user, { new: false });
+    }
+
+    async delete(id: string): Promise<IRoom | null> {
+        return await this.roomModel.findOneAndDelete({ _id: id });
+    }
+}
