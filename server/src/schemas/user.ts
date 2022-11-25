ccad02d87ebc979fe14e980bb37131268c05ee33
Thomasparsley <info@thomasparsley.cz>
2022-11-25 14:33:25 +0100

    user schema update

--- server/src/schemas/user.ts
+++ server/src/schemas/user.ts
@@ -1,4 +1,6 @@
-import { Schema } from 'mongoose';
+import { Schema, model } from 'mongoose';
+import { randomBytes, pbkdf2Sync } from 'crypto';
+import { env } from 'process';
 
 export interface IUser {
     username: String;
@@ -7,8 +9,8 @@ export interface IUser {
     lastName: String;
     passwordHash: String;
     salt: String;
-    permissions: UserPermissions;
-}
+    permissions?: Number;
+};
 
 const userSchema = new Schema<IUser>({
     username: { type: String, required: true },
@@ -17,12 +19,34 @@ const userSchema = new Schema<IUser>({
     lastName: { type: String, required: true },
     passwordHash: { type: String, required: true },
     salt: { type: String, required: true },
-    permissions: { type: UserPermissions[], }
-})
+    permissions: { type: Number, required: false }
+});
+
+userSchema.methods.generatePasswordHash = function (plainPassword: string): Promise<void> {
+    return new Promise((resolve, reject) => {
+        this.salt = randomBytes(+((process.env.SALT_LENGTH) ?? 64)).toString("hex");
+        try {
+            const key = pbkdf2Sync(plainPassword, this.salt, 100000, 512, 'sha512');
+            this.passwordHash = key;
+            resolve();
+        }
+        catch (err) {
+            reject(err);
+        }
+    });
+};
+
+userSchema.methods.verifyPassword = async function (plainPassword: string): Promise<boolean> {
+
+}
+
+
 
 export enum UserPermissions {
     MANAGE_ROOMS = 1 << 1,
     MANAGE_TABLES = 1 << 2,
     SET_TABLE_FEATURES = 1 << 3,
     MANAGE_TABLE_FEATURES = 1 << 4
-}
+};
+
+export const UserModel = model<IUser>("User", userSchema);
