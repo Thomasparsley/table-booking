8e16e4fc12d3516ceb8c9fbffc723f1c7b7a15ba
Thomasparsley <info@thomasparsley.cz>
2022-11-26 07:06:44 +0100

    auto token renew

--- client/layouts/default.vue
+++ client/layouts/default.vue
@@ -1,3 +1,23 @@
+<script setup lang="ts">
+
+let authTokenRenew: any = null;
+
+onMounted(() => {
+    authTokenRenew = setInterval(() => {
+        fetch("/api/auth/renew", {
+            method: "POST",
+            credentials: "include",
+        });
+    }, 1000 * 60 * 5);
+});
+
+onUnmounted(() => {
+    clearInterval(authTokenRenew);
+});
+
+</script>
+
+
 <template>
     <div class="relative min-h-screen">
         <NavNavbar />
